206e8ecbb1f72a8add5ce9b3483e96a7e3959362
CodeSandbox <support@codesandbox.io>
2026-05-13 09:54:43 +0000

    Sandbox snapshot commit

---
+++ app/components/Counter.js
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+export default function Counter() {
+  const [count, setCount] = React.useState(0);
+
+  return <button onClick={() => setCount(count + 1)}>Count: {count}</button>;
+}
